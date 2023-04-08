c1bd403a0eaf4cf1ae7bde2dc481945bf4fb85b0
Ghimirevivek <stephengmre@gmail.com>
2023-04-08 15:37:38 +0530

    m

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,41 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import '../styles/App.css'
 
-class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+function Home() {
+  return <h1>You are home.</h1>
 }
 
+function About() {
+  return <h1>You are on the about page.</h1>
+}
 
-export default App;
+function App() {
+  return (
+    <div id="main">
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+
+export default App
