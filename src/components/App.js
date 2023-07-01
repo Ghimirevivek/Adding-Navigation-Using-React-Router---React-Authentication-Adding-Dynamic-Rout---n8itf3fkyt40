c96e60b3a985e075e55e79db4f75e962f5db2c59
vivek <stephengmre@gmail.com>
2023-07-01 19:54:09 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,46 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import '../styles/App.css'
-import LocationDisplay from './LocationDisplay'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import '../styles/App.css';
+import LocationDisplay from './LocationDisplay';
 
 function Home() {
-  return <h1>You are home.</h1>
+  return <h1>You are home.</h1>;
 }
 
 function About() {
-  return <h1>You are on the about page.</h1>
+  return <h1>You are on the about page.</h1>;
+}
+function NotFound() {
+  return <h2>Not Found</h2>;
 }
 function App() {
   return (
-    <div id="main">
+    <div id='main'>
       <Router>
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to='/'>Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to='/about'>About</Link>
             </li>
           </ul>
         </nav>
 
         <Switch>
-          <Route exact path="/">
+          <Route exact path='/'>
             <Home />
           </Route>
-          <Route path="/about">
+          <Route path='/about'>
             <About />
           </Route>
-          <Route path="*">{<h2>NotFound</h2>}</Route>
+          <Route path='*'>{<NotFound />}</Route>
         </Switch>
         <LocationDisplay />
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
